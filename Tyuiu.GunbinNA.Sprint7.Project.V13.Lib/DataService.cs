d4e8c0bd83c4aa310fc0df5a9762682af630db01
Tyuiu.GunbinNA@outlook.com <gunbin1005@gmail.com>
2023-12-16 16:26:20 +0500

    Add project files.

---
+++ Tyuiu.GunbinNA.Sprint7.Project.V13.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Tyuiu.GunbinNA.Sprint7.Project.V13.Lib
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Tyuiu.GunbinNA.Sprint7.Project.V13.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GunbinNA.Sprint7.Project.V13.Lib;
+namespace Tyuiu.GunbinNA.Sprint7.Project.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
