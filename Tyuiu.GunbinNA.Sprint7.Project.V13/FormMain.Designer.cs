71827f02e19a38177a61740a678c352e750c3fa9
Tyuiu.GunbinNA@outlook.com <gunbin1005@gmail.com>
2023-12-17 22:39:26 +0500

    woOOOW

--- Tyuiu.GunbinNA.Sprint7.Project.V13/FormMain.Designer.cs
+++ Tyuiu.GunbinNA.Sprint7.Project.V13/FormMain.Designer.cs
@@ -29,21 +29,100 @@ namespace Tyuiu.GunbinNA.Sprint7.Project.V13
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelButtons = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panelButtons.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(1104, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 45);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panelButtons
+            // 
+            this.panelButtons.BackColor = System.Drawing.Color.Lime;
+            this.panelButtons.Controls.Add(this.panel1);
+            this.panelButtons.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelButtons.Location = new System.Drawing.Point(0, 0);
+            this.panelButtons.Name = "panelButtons";
+            this.panelButtons.Size = new System.Drawing.Size(313, 617);
+            this.panelButtons.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(313, 126);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mistral", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Coral;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.MinimumSize = new System.Drawing.Size(265, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 75);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TyuiuGeoGraphics";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Location = new System.Drawing.Point(1104, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 45);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(255)))), ((int)(((byte)(150)))));
+            this.ClientSize = new System.Drawing.Size(1161, 617);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panelButtons);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.panelButtons.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelButtons;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Tyuiu.GunbinNA.Sprint7.Project.V13/FormMain.cs
+++ Tyuiu.GunbinNA.Sprint7.Project.V13/FormMain.cs
@@ -13,14 +13,36 @@ namespace Tyuiu.GunbinNA.Sprint7.Project.V13
 {
     public partial class FormMain : Form
     {
+        WMPLib.WindowsMediaPlayer music = new WMPLib.WindowsMediaPlayer();
+        string path;
+        bool isPlaying = false;
         public FormMain()
         {
             InitializeComponent();
+            music.URL = @"C:\Users\wackko\Desktop\ListSongs.mp3";
+            music.settings.volume = 10;
+            music.controls.play();
         }
 
-        private void FormMain_Load(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
+            Application.Exit();
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (isPlaying)
+            {
+                music.controls.stop();
+                button2.BackColor = Color.Red;
+            }
+            else
+            {
+                music.controls.play();
+                button2.BackColor = Color.FromArgb(192, 255, 192);
+            }
+
+            isPlaying = !isPlaying;
         }
     }
 }
